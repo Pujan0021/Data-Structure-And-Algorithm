89e42a4e36a56d7071cca988118fd381b7f15fc4
Pujan-Pandey <pujanpandey21@gmail.com>
2025-12-31 06:59:58 +0545

    Infix To PostFix

--- Stack/infixToPostfixConversion.java
+++ Stack/infixToPostfixConversion.java
@@ -74,5 +74,6 @@ public class infixToPostfixConversion {
         String infixExpressionInUpperCase = infix.toUpperCase();
         System.out.println("Infix: " + infixExpressionInUpperCase);
         System.out.println("Postfix: " + convertToPostFix(infixExpressionInUpperCase));
+        infixExp.close();
     }
 }
